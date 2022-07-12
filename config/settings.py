dd82449b4a33b31e0fcd76971592c1dfe62cf511
murry2018 <2clean8@naver.com>
2022-07-12 19:09:38 +0900

    protect secret key

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$mlimrj-)xdt&nh033gdygyba=e^)q6)3$u(s66ino$a28%7@_'
+try:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+except KeyError:
+    SECRET_KEY = 'django-insecure-$mlimrj-)xdt&nh033gdygyba=e^)q6)3$u(s66ino$a28%7@_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
